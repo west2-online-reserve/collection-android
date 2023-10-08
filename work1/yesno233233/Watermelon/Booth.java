70629a47d0bd76521db92e3c2f00901e805946b8
WangXinhong <1324494585@qq.com>
2023-10-08 20:13:36 +0800

    complete work1

--- work1/yesno233233/Watermelon/Booth.java
+++ work1/yesno233233/Watermelon/Booth.java
@@ -15,17 +15,36 @@ public class Booth {
     }
 
     //静态方法：购买purchase
-    public static void purchase(int purchaseNum, int total, boolean isClosed) {
-        if (purchaseNum > total || purchaseNum <= 0 || isClosed ) {
-            System.out.println("购买失败。"
+    public static void purchase(Booth seller,int purchaseNum) {
+        long id=seller.getId();
+        String name= seller.getName();
+        int total=seller.getTotal();
+        boolean isClosed=seller.isClosed();
+        if (purchaseNum > total ) {
+            System.out.println("你在id为"+id+"，名字叫"+name+"的西瓜摊购买失败。"
                     + "购买的西瓜数为" + purchaseNum
                     + "在售西瓜数为" + total
                     + "是否关门状态：" + isClosed
-                    + "请确保购买的西瓜数必须为正数；商家不能处于休摊整改状态；购买西瓜数不能大于在售西瓜数。");
-            return;
-        }
+                    + "购买西瓜数不能大于在售西瓜数。");
+                     return;
+             } else if ( purchaseNum <= 0) {
+                    System.out.println("你在id为"+id+"，名字叫"+name+"的西瓜摊购买失败。"
+                    + "购买的西瓜数为" + purchaseNum
+                    + "在售西瓜数为" + total
+                    + "是否关门状态：" + isClosed
+                    + "购买的西瓜数必须为正数.");
+                    return;
+                      } else if (isClosed ) {
+                            System.out.println("你在id为"+id+"，名字叫"+name+"的西瓜摊购买失败。"
+                             + "购买的西瓜数为" + purchaseNum
+                             + "在售西瓜数为" + total
+                             + "是否关门状态：" + isClosed
+                             + "商家不能处于休摊整改状态。");
+                            return;
+                        }
         total = total - purchaseNum;
-        System.out.println("购买成功"
+        seller.setTotal(total);
+        System.out.println("你在id为"+id+"，名字叫"+name+"的西瓜摊购买成功。"
                 + "成功购买的西瓜数为" + purchaseNum
                 + "摊位剩余西瓜数为" + total);
     }
@@ -67,13 +86,19 @@ public class Booth {
 
     //实例方法：进货restock
     public void restock(int restockNum) {
-        if (restockNum <= 0 || restockNum > 200) {
+        if (restockNum <= 0 ) {
             System.out.println("进货失败" +
-                    "你的进货量为" + restockNum
+                    "你的进货量为" + restockNum+" <=0"
+                    + "你的进货物应该在1~200之间");
+            return;
+        }else if (restockNum > 200) {
+            System.out.println("进货失败" +
+                    "你的进货量为" + restockNum+" >200"
                     + "你的进货物应该在1~200之间");
             return;
         }
         total=total+restockNum;
+        this.setTotal(total);
         System.out.println("成功进货"+restockNum+"个西瓜"
                             +"现在摊位共有"+total+"个西瓜在售");
     }
@@ -83,7 +108,10 @@ public class Booth {
         for (Booth booth : booths) {
             if (!booth.isClosed()) {
                 booth.setClosed(true);
-                System.out.println("摊位已休业整改：" + booth.toString());
+                System.out.println("摊位成功转为休业整改");
+            }
+            else{
+                System.out.println("摊位本就在修业整改："+booth.toString());
             }
         }
     }
@@ -91,7 +119,7 @@ public class Booth {
     public String toString() {
         return "摊位信息：" +
                 "摊号=" + id +
-                ", 摊主姓名：'" + name  +
+                ", 摊主姓名：'" + name+"'"  +
                 ", 在售西瓜数：" + total +
                 ", 是否休摊整改：" + isClosed;
     }
